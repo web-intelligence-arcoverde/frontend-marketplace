b0e3b5a8652958bbd25b01c4eeacb82e468bf1f7
Lucas Henrique <lucashenrique@Mac-Pro-de-Lucas.local>
2022-03-09 12:41:26 -0300

    ajustando

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,48 +1,53 @@
 /* eslint-disable arrow-body-style */
-import React , { useEffect, useState }  from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 
 import { Dock } from 'react-dock';
 import Product from '../product/list';
 import './style.css';
 
-
 function sidebar() {
-  const history = useHistory();
   const { cart } = useSelector((state) => state.shop);
-  const total =  cart.reduce((max , product) => {return max + product.preco;},0);
-  const  [opened, setOpened ]= useState(false);
+  const total = cart.reduce((max, product) => {
+    return max + product.preco;
+  }, 0);
+  const [opened, setOpened] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     window.addEventListener('openCart', () => {
       setOpened(true);
     });
-  },[]);
-
+  }, []);
 
   return (
-  <Dock
-  position='right' 
-  isVisible={opened}
-  onVisibleChange={(visible)=>{setOpened(visible);}}
-  >
-    <div className='container h-100 pt-4 sidebar'> 
-      <h5>Minha sacola ({cart.length})</h5>
-      <div className='row products'>
-         {cart.map((p) => (
-        <Product Product={p} />
-        ))} 
-      </div>
-      <div className='row footer align-items-end'>
-        <div className='col-12 d-flex justify-content-between align-items-center'>
-          <b className='d-inline-block'>Total</b>
-          <h3 className='d-inline-block'>R$ {total.toFixed(2)}</h3>
+    <Dock
+      position="right"
+      isVisible={opened}
+      onVisibleChange={(visible) => {
+        setOpened(visible);
+      }}
+    >
+      <div className="container h-100 pt-4 sidebar">
+        <h5>Minha sacola ({cart.length})</h5>
+        <div className="row products">
+          {cart.map((p) => (
+            <Product Product={p} />
+          ))}
         </div>
-        <button onClick={() => history.push('/checkout')} type='button' className='btn btn-block btn-lg btn btn-primary rounded-0 h-50 align-items-center' >Finalizar compra</button>
+        <div className="row footer align-items-end">
+          <div className="col-12 d-flex justify-content-between align-items-center">
+            <b className="d-inline-block">Total</b>
+            <h3 className="d-inline-block">R$ {total.toFixed(2)}</h3>
+          </div>
+          <button
+            type="button"
+            className="btn btn-block btn-lg btn btn-primary rounded-0 h-50 align-items-center"
+          >
+            Finalizar compra
+          </button>
+        </div>
       </div>
-    </div>
-  </Dock>
+    </Dock>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,22 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 import React from 'react';
-import { BrowserRouter , Routes, Route} from 'react-router-dom'
-import Sidebar from './components/sidebar/index';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cadastro from './pages/cadastro/index';
 import Checkout from './pages/checkout/index';
 import PetShop from './pages/petshop/index';
 import Home from './pages/home';
 import './styles/global.css';
 
-function Router(){
-  return ( 
-<>
-  <BrowserRouter>
-    <Routes>
-      <Route exact path='/' element={<Home/>} />
-      <Route exact path='/petshop/:id' element={<PetShop/>} />
-      <Route exact path='/checkout' element={<Checkout/>} />
-      <Route exact path='/cadastro' element={<Cadastro/>} />
-      <Sidebar />
-    </Routes>
-  </BrowserRouter>
-</>
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/petshop/:id" element={<PetShop />} />
+        <Route exact path="/checkout" element={<Checkout />} />
+        <Route exact path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
-export default Router
+}
+export default Router;
