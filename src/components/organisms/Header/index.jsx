ff5210a780679c8897a133c056a4ac3e53713ba3
Lucas Henrique <lucashenrique@Mac-Pro-de-Lucas.local>
2022-03-14 12:55:27 -0300

    merge into master

--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -17,22 +17,29 @@ function Header({ hideCart }) {
 
   return (
     <div className="col-12">
-      <header className="py-4 px-4 text-center">
-        <img src={Logo} className="img-fluid" alt="dffsdf" />
-      </header>
-      {!hideCart && (
-        <button
-          type="button"
-          onClick={() => {
-            openDrawer();
-          }}
-          className="btn btn-secondary cart-button"
-        >
-          <span className="mdi mdi-cart" />
-          {cart.length}
-          items
-        </button>
-      )}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        <header className="py-4 px-4 text-center">
+          <img src={Logo} className="img-fluid" alt="dffsdf" />
+        </header>
+        {!hideCart && (
+          <button
+            type="button"
+            onClick={() => {
+              openDrawer();
+            }}
+            className="btn btn-secondary cart-button"
+          >
+            <span className="mdi mdi-cart" />
+            {cart.length} items
+          </button>
+        )}
+      </div>
     </div>
   );
 }
