c0996f938e7db25ec538bb4b6ea10e387ed0b971
Kevin <k.w9cavalcanti@gmail.com>
2022-03-14 10:07:16 -0300

    ProductLogo criado e products centralizados

---
+++ src/components/molecules/ProductLogo/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Container } from './styled';
+
+function ProductLogo() {
+  return( 
+  <Container>
+    <div className="col-3">
+      {/* <img src={shop.logo} alt="petlovelogo" />
+      <b>{shop.nome}</b>
+      <div className="petshop-infos">
+        <span className="mdi mdi-star" />
+        <text>
+          <b>2,8</b>
+        </text>
+        <span className="mdi mdi-cash-multiple" />
+        <text>{shop.categoria}</text>
+        <span className="mdi mdi-crosshairs-gps" />
+        <text>2.9km</text>
+      </div>
+      <label className="badge-secondary">Frete gratis</label> */}
+    </div>
+  </Container>
+  );
+}
+
+export default ProductLogo;
--- src/pages/Market/index.jsx
+++ src/pages/Market/index.jsx
@@ -24,21 +24,6 @@ function Market() {
       <Header />
       <div className="container ">
         <div className="row">
-          <div className="col-3">
-            <img src={shop.logo} alt="petlovelogo" />
-            <b>{shop.nome}</b>
-            <div className="petshop-infos">
-              <span className="mdi mdi-star" />
-              <text>
-                <b>2,8</b>
-              </text>
-              <span className="mdi mdi-cash-multiple" />
-              <text>{shop.categoria}</text>
-              <span className="mdi mdi-crosshairs-gps" />
-              <text>2.9km</text>
-            </div>
-            <label className="badge-secondary">Frete gratis</label>
-          </div>
           <div className="col-9">
             <h5>Produtos</h5>
             <div className="row">
