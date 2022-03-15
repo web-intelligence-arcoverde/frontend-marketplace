d2556597d9d7cb8f24b729fedfa70c15cab5d382
lucaspaes <lukas.paes18@gmail.com>
2022-03-15 12:53:30 -0300

    Adicionado vinculo do Redux na signin

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { CardButton } from './styles';
 
-const Button =({title,onClick,img})=> {
+const Button = ({ title, onClick, img }) => {
   return (
     <CardButton>
-      <button type='button' 
-      onClick={onClick}>
+      <button type="submit" onClick={onClick}>
         <img src={img} alt="" />
         {title}
       </button>
     </CardButton>
-  )
-}
+  );
+};
 
 export default Button;
